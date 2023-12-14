a6f795a9b330e53461e0af42464f901acb5fee74
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2023-12-14 12:50:18 +0530

    Removing static

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
-app.use('/api/v1/tasks', tasks)
+app.use('/api/v1/tasks', tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => console.log(`Listening on port ${port} ...`));
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Listening on port ${port} ...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
